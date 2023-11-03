c14120b5aa777675f8be60dfe016c0ab4f7e406d
vigo walker <59869868+theshadow76@users.noreply.github.com>
2023-11-03 17:13:35 -0300

    Add files via upload

---
+++ tensorflow 2.0 crash course - freecodecamp/Tutorial 1.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+from tensorflow import keras 
+import numpy as np 
+import matplotlib.pyplot as plt
+
+data = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = data.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+#plt.imshow(train_images[7], cmap=plt.cm.binary)
+#plt.show()
+
+train_images = train_images/255.0
+test_images = test_images/255.0
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28,28)),
+    keras.layers.Dense(128, activation="relu"),
+    keras.layers.Dense(10, activation="softmax")
+])
+
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+model.fit(train_images, train_labels, epochs=5)
+
+#test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+#print("Tested Acc: ", test_acc)
+
+prediction = model.predict(test_images)
+#print(class_names[np.argmax(prediction[0])])
+
+for i in range(5):
+    plt.grid(False)
+    plt.imshow(test_images[i], cmap=plt.cm.binary)
+    plt.xlabel("Actual: " + test_labels[i])
+    plt.title("prediction" + class_names[np.argmax(prediction[i])])
+    plt.show()
+
