e020e3db06ecf484b0bc6e3320e4b4a34b391228
vigo walker <59869868+theshadow76@users.noreply.github.com>
2023-11-03 17:26:49 -0300

    Add files via upload

---
+++ pendrives/automatisacions/code 1.py
@@ -0,0 +1,7 @@
+import pyautogui
+
+print("\tBienvenido a ShadowBot")
+print("1. enviar mensages por instagram automaticamente")
+print("2. comprar por amazon automaticamente")
+print("3. convertir datos a exel")
+
